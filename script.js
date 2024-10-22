b5daa65072f6682a21f47143b9d2c6b608276c05
euno4h <163136318+euno4h@users.noreply.github.com>
2024-10-22 09:26:17 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function alterarCirculo(){
     const circulo = document.getElementById('circulo');
 
-    const corAleatoria = '#' + math.floor(math.random()*16777215).toString(16);
+    const corAleatoria = '#' + 
+    math.floor(math.random()*16777215).toString(16);
     
     const novoTamanho = math.floor(math.random()*(200 - 50 + 1))+ 50;
 
